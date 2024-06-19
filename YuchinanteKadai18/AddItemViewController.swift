ceb7ae17baaf4c398ac352ad1b29598a3d2d8296
yuya-3110 <ysuu45143745@gmail.com>
2024-06-19 21:34:05 +0900

    レビュー後修正

--- YuchinanteKadai18/AddItemViewController.swift
+++ YuchinanteKadai18/AddItemViewController.swift
@@ -11,8 +11,26 @@ import UIKit
 class AddItemViewController: UIViewController {
 
     // 追加モードと編集モードを定義する列挙体
-    enum Mode: Equatable {
-        case add, edit
+    enum Mode {
+        case add, edit(TableViewController.Item)
+
+        var saveButtonSegueIdentifier: String {
+            switch self {
+            case .add:
+                return "exitFromAddBySaveSegue"
+            case .edit:
+                return "exitFromEditBySaveSegue"
+            }
+        }
+
+        var cancelButtonSegueIdentifier: String {
+            switch self {
+            case .add:
+                return "exitFromAddByCancelSegue"
+            case .edit(let oldItem):
+                return "exitFromEditByCancelSegue"
+            }
+        }
     }
 
     // デフォルトは追加モード
@@ -22,33 +40,40 @@ class AddItemViewController: UIViewController {
     @IBOutlet weak var nameTextField: UITextField!
 
     // 入力されたアイテム名を保持するプロパティ
-    var name = ""
+    private(set) var editedItem: TableViewController.Item?
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // 変種モードの場合はテキストフィールドに既存のアイテム名を表示する
-        if mode == .edit {
-            nameTextField.text = name
+        switch mode {
+        case .add:
+            break
+        case .edit(let item):
+            nameTextField.text = item.name
         }
     }
 
     // セーブボタンが押された時の処理
     @IBAction func pressSaveButton(_ sender: Any) {
-        // テキストフィールドの値をnameプロパティに設定
-        name = nameTextField.text ?? ""
-        // モードに応じて適切なセグエ識別子を設定
-        let identifier = (mode == .add) ? "exitFromAddBySaveSegue" : "exitFromEditBySaveSegue"
+        let isChecked: Bool
+
+        switch mode {
+        case .add:
+            isChecked = false
+        case .edit(let oldItem):
+            isChecked = oldItem.isChecked
+        }
+
+        editedItem = .init(name: nameTextField.text ?? "", isChecked: isChecked)
+
         // セグエを実行して遷移する
-        performSegue(withIdentifier: identifier, sender: sender)
+        performSegue(withIdentifier: mode.saveButtonSegueIdentifier, sender: sender)
     }
-    
 
     // キャンセルボタンが押された時の処理
     @IBAction func pressCancelButton(_ sender: Any) {
-        // モードに応じて適切なセグエ識別子を設定
-        let identifier = (mode == .add) ? "exitFromAddByCancelSegue" : "exitFromEditByCancelSegue"
         // セグエを実行して遷移する
-        performSegue(withIdentifier: identifier, sender: sender)
+        performSegue(withIdentifier: mode.cancelButtonSegueIdentifier, sender: sender)
     }
 }
